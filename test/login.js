9b5b644c5aec900307fbaad150ede30518ab563c
rquiroz <rquiroz3290@gmail.com>
2023-08-05 23:26:45 -0400

    subbing hardcoded creds for environment variables and updating gitignore

--- test/login.js
+++ test/login.js
@@ -2,26 +2,29 @@ import { Selector } from "testcafe";
 import LoginPage from "../pageObjects/loginPage"
 import basePage from "../pageObjects/basePage";
 
+ require("dotenv").config()
+
 fixture `Login Suite` 
 .page("https://www.saucedemo.com")
 
 test("Can login to demo site", async (t)=>{
-    await LoginPage.login("standard_user","secret_sauce")
+    await LoginPage.login(process.env.validUsername, process.env.password)
     await t
     .expect(Selector(".title").innerText).eql("Products")
 });
 
 
 test("Can login and logout to demo site", async (t)=>{
-    await LoginPage.login("standard_user","secret_sauce")
+    await LoginPage.login(process.env.validUsername, process.env.password)
     await basePage.logout()
     await t
     .expect(LoginPage.usernameInput.innerText).eql("")
 });
 
 test.skip("This Should Fail", async (t)=>{
-    await LoginPage.login("standard_user","secret_sauce")
-    await basePage.logout()
+    console.log(process.env.validUsername)
+    await LoginPage.login(process.env.validUsername, process.env.password)
+    await basePage.logout()    
     await t
     .expect(LoginPage.usernameInput.innerText).eql("test")
 });
